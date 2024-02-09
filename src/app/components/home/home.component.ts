47248851f9e2d1a1d170b06badc50c7dbf8ca70a
vascsx <vasczn@gmail.com>
2024-02-09 19:44:07 -0300

    feat : adicionei estrutura html do especialidades componente

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { EspecialidadesComponent } from '../especialidades/especialidades.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, EspecialidadesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
