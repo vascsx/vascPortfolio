527eab3131046350730b991c110fa7333cb14845
vascsx <vasczn@gmail.com>
2024-02-10 13:24:52 -0300

    feat: adicionei o componente sobre e fiz a estilização.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { EspecialidadesComponent } from '../especialidades/especialidades.component';
+import { SobreComponent } from '../sobre/sobre.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, EspecialidadesComponent],
+  imports: [HeaderComponent, EspecialidadesComponent,SobreComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
