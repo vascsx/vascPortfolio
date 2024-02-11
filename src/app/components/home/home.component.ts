be186e71e73a6d8235975f674059b634f33ed94e
vascsx <vasczn@gmail.com>
2024-02-10 23:36:01 -0300

    estrutura html do componente portfolio

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { EspecialidadesComponent } from '../especialidades/especialidades.component';
 import { SobreComponent } from '../sobre/sobre.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, EspecialidadesComponent,SobreComponent],
+  imports: [HeaderComponent, EspecialidadesComponent,SobreComponent, PortfolioComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
