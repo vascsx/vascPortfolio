b8d5c7ecbb2e3832be68426f6f2b0c6c18df7a19
vascsx <vasczn@gmail.com>
2024-03-25 20:52:48 -0300

    feat: add typescript no component especialidades

--- src/app/components/especialidades/especialidades.component.ts
+++ src/app/components/especialidades/especialidades.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-especialidades',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './especialidades.component.html',
   styleUrl: './especialidades.component.scss'
 })
 export class EspecialidadesComponent {
-
+  tecnologias = [
+    { nome: 'Angular', icone: 'assets/icons-especialidades/angular.svg' },
+    { nome: 'Java', icone: 'assets/icons-especialidades/java.svg' },
+    { nome: 'Typescript', icone: 'assets/icons-especialidades/typescript.svg' },
+    { nome: 'Mysql', icone: 'assets/icons-especialidades/mysql.svg' },
+    { nome: 'Scss', icone: 'assets/icons-especialidades/sass.svg' },
+    { nome: 'Php', icone: 'assets/icons-especialidades/php.svg' }
+  ];
 }
